2e71af6a0f39e4f8d2b0fa9e91325f56dbec8642
ODKhang <ondinhk@gmail.com>
2022-10-10 01:51:00 +0700

    fix error not comment

--- page.py
+++ page.py
@@ -20,8 +20,8 @@ logger = logging.getLogger()
 # Now we are going to Set the threshold of logger to DEBUG
 logger.setLevel(logging.INFO)
 # Waiting 20s for page load
-DELAY = 3
-DELAY_POPUP = 2
+DELAY = 2
+DELAY_POPUP = 1
 
 # Skip string
 def start_thread():
@@ -59,14 +59,14 @@ def crawl(data, category, name):
     temp = []
     chrome = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
     for idx, item in enumerate(data):
-    	try:
+        try:
             temp.append(get_content_page(item, idx, category, chrome=chrome))
         except:
             continue
     export_data_to_json(name, temp)
 
 
-def get_content_page(link, idx, category, chrome):
+def get_content_page(link, idx, idLocation, chrome):
     global DELAY
     chrome.get(link)
     sleep(DELAY)
@@ -75,18 +75,23 @@ def get_content_page(link, idx, category, chrome):
     # query
     q_title = soup.find('h2', class_='d2fee87262 pp-header__title')
     q_description = soup.find('div', class_='hp_desc_main_content')
+    q_address = soup.find('span', class_='hp_address_subtitle js-hp_address_subtitle jq_tooltip')
     q_number_comment = soup.find('span', class_='b5cd09854e c90c0a70d3 db63693c62')
     q_rate = soup.find('div', class_='b5cd09854e d10a6220b4')
     q_label = soup.find('span', class_='b5cd09854e f0d4d6a2f5 e46e88563a')
-    q_address = soup.find('span', class_='hp_address_subtitle js-hp_address_subtitle jq_tooltip')
     # get text
     try:
         title = q_title.get_text(strip=True)
         description = q_description.get_text(strip=True).replace('Tôi muốn xem thêm', '')
-        number_comment = q_number_comment.get_text(strip=True).replace('·','')
-        rate = q_rate.get_text(strip=True)
-        label_rate = q_label.get_text(strip=True)
         address = q_address.get_text(strip=True)
+        try:
+            number_comment = q_number_comment.get_text(strip=True).replace('·','')
+            rate = q_rate.get_text(strip=True)
+            label_rate = q_label.get_text(strip=True)
+        except:
+            number_comment = "Chưa có đánh giá"
+            rate = '0'
+            label_rate = '0'
         try:
             q_cost_original = soup.find_all('div',
                                             class_='bui-f-color-destructive js-strikethrough-price prco-inline-block-maker-helper bui-price-display__original')
@@ -97,9 +102,9 @@ def get_content_page(link, idx, category, chrome):
         except:
             try:
                 q_cost_original = soup.find_all('div',
-                                                class_='bui-price-display__value prco-text-nowrap-helper prco-inline-block-maker-helper prco-f-font-heading ')
-                cost_original = cost_original = q_cost_original[0].get_text(strip=True)
-                cost_new = '0'
+                                                class_='bui-price-display__value prco-text-nowrap-helper prco-inline-block-maker-helper prco-f-font-heading')
+                cost_original = cost_original = q_cost_original[0].get_text(strip=True).replace('\xa0',' ')
+                cost_new = 'Hiện chưa có khuyến mãi'
             except:
                 cost_original = 'Update'
                 cost_new = 'Update'
@@ -108,18 +113,18 @@ def get_content_page(link, idx, category, chrome):
         except Exception as e:
             logger.error("{} - {} - {}".format(idx, 'Cannot get images', title))
             return
+        logger.info("{} - {} - {}".format(idx, 'Done', title))
         object = {'idHouse': idx,
+                  'idLocation': idLocation,
                   'name': title,
                   'address': address,
-                  'images': images,
                   'costOriginal': cost_original,
                   'costNew': cost_new,
                   'numberOfComment': number_comment,
                   'rate': rate,
                   'label': label_rate,
-                  'category': category,
-                  'description': description}
-        logger.info("{} - {} - {}".format(idx, 'Done', title))
+                  'description': description,
+                  'images': images}
         return object
     except Exception as e:
         logger.error("{} - {} - {}".format(idx, e, link))
@@ -176,8 +181,15 @@ def test_file_json():
     print(a)
     print(b)
 
+def test_get_page():
+    link = 'https://www.booking.com/hotel/vn/delen-house-thanh-pho-da-lat4.vi.html?aid=304142&label=gen173nr-1DCAMo9AE40wNIKlgEaPQBiAEBmAEquAEZyAEM2AED6AEB-AECiAIBqAIDuAKC_7yZBsACAdICJGE5MTMyZTJlLTVjYjEtNGQ2Yy04Y2RhLWZmYTJiZjcyZWIzYtgCBOACAQ&sid=87b657754f8327f14759b75311b5a649&all_sr_blocks=906463701_361730965_2_0_0;checkin=2022-10-10;checkout=2022-10-11;dest_id=-3712045;dest_type=city;dist=0;group_adults=2;group_children=0;hapos=8;highlighted_blocks=906463701_361730965_2_0_0;hpos=8;matching_block_id=906463701_361730965_2_0_0;no_rooms=1;req_adults=2;req_children=0;room1=A%2CA;sb_price_type=total;sr_order=popularity;sr_pri_blocks=906463701_361730965_2_0_0__25000000;srepoch=1664048266;srpvid=55708a04d70d027f;type=total;ucfs=1&#hotelTmpl'
+    idx = 0
+    category = 1
+    chrome = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+    print(get_content_page(link, idx, category, chrome))
+
 if __name__ == '__main__':
-    # test_get_page()
+    test_get_page()
     # test_image()
     # test_file_json()
-    start_thread()
+    # start_thread()
